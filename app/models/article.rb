7f3569cb38a02a58ef226d4e1d71a1b20d80ef9a
prvn-codes <praveensampath2706@gmail.com>
2022-12-09 14:43:08 +0530

    added security (basic authentication)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,16 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence: true, length: { minimum: 10 }
+    include Visible
+
+    has_many :comments, dependent: :destroy
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
+
+    VALID_STATUSES = ['public', 'private', 'archived']
+
+    validates :status, inclusion: { in: VALID_STATUSES }
+
+    def archived?
+        status == 'archived'
+    end
 end
